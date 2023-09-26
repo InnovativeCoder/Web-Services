ce94cd8ac836982ab2cc7b63d91247255a2e1f64
InnovativeCoder <aashitsingh2010@gmail.com>
2023-09-26 12:48:50 -0400

    Sept 26th 2023

---
+++ soap/src/test/java/dev/jasneet/soap/SoapApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jasneet.soap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
