e9b2a523f15f041cc5cb69c9fbc6482f872a0057
InnovativeCoder <aashitsingh2010@gmail.com>
2023-10-02 12:57:16 -0400

    rest uploaded

---
+++ restful/src/main/java/dev/jerry/restful/HelloWorldBean.java
@@ -0,0 +1,24 @@
+package dev.jerry.restful;
+
+public class HelloWorldBean {
+    public String message;
+
+    public HelloWorldBean(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "HelloWorldBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
---
+++ restful/src/main/java/dev/jerry/restful/HelloWorldController.java
@@ -0,0 +1,5 @@
+package dev.jerry.restful;
+
+public class HelloWorldController {
+
+}
