20746f47065b03b7ff222e250373f34ae59219b4
InnovativeCoder <aashitsingh2010@gmail.com>
2023-10-05 13:21:28 -0400

    Added internationalisation

--- restful/src/main/java/dev/jerry/restful/HelloWorld.java
+++ restful/src/main/java/dev/jerry/restful/HelloWorld.java
@@ -1,11 +1,22 @@
 package dev.jerry.restful;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorld {
+
+    private MessageSource messageSource;
+
+    public HelloWorld(MessageSource messageSource){
+        this.messageSource = messageSource;
+    }
+
     @GetMapping(path = "/hello-world")
     public String helloworld(){
         return "Hello World";
@@ -19,5 +30,11 @@ public class HelloWorld {
     @GetMapping(path = "/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
         return new HelloWorldBean("Hello World "+name);
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(){
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
     }
 }
