48d53d7d74fa2ba52911a4cf0a9c62234eec6f37
InnovativeCoder <aashitsingh2010@gmail.com>
2023-10-23 20:04:14 -0400

    Optimising tests with mockito annocations

---
+++ Test - JUnit and Mockito/testing/src/test/java/dev/jerry/testing/ListMockTest.java
@@ -0,0 +1,42 @@
+package dev.jerry.testing;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ListMockTest {
+
+    List mock = mock(List.class);
+
+    @Test
+    public void size_basic(){
+        when(mock.size()).thenReturn(5);
+        assertEquals(5, mock.size());
+    }
+
+    @Test
+    public void returnDifferentValues(){
+        when(mock.size()).thenReturn(5).thenReturn(10);
+        assertEquals(5, mock.size());
+        assertEquals(10, mock.size());
+    }
+
+    @Test
+    public void returnWithParameters(){
+        when(mock.get(0)).thenReturn("Jerry");
+        assertEquals("Jerry", mock.get(0));
+        assertEquals(null, mock.get(1));
+    }
+
+    @Test
+    public void returnWithGenericParameters(){
+        when(mock.get(anyInt())).thenReturn("Jerry");
+        assertEquals("Jerry", mock.get(0));
+        assertEquals("Jerry", mock.get(1));
+    }
+}
--- Test - JUnit and Mockito/testing/src/test/java/dev/jerry/testing/SomeBusinessMockTest.java
+++ Test - JUnit and Mockito/testing/src/test/java/dev/jerry/testing/SomeBusinessMockTest.java
@@ -4,26 +4,37 @@ import dev.jerry.testing.Business.SomeBusinessImpl;
 import dev.jerry.testing.data.SomeDataService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class SomeBusinessMockTest {
-    class SomeDataServiceStub implements SomeDataService{
-        @Override
-        public int[] retrieveAllData(){
-            return new int[]{1,2,3};
-        }
-    }
+//    class SomeDataServiceStub implements SomeDataService{
+//        @Override
+//        public int[] retrieveAllData(){
+//            return new int[]{1,2,3};
+//        }
+//    }
 
+    @InjectMocks
     SomeBusinessImpl business = new SomeBusinessImpl();
-    SomeDataService dataServiceMock = mock(SomeDataService.class);
 
-    @BeforeEach
-    public void before(){
-        business.setSomeDataService(dataServiceMock);
-    }
+    @Mock
+    SomeDataService dataServiceMock;
+
+//    @BeforeEach
+//    public void before(){
+//        business.setSomeDataService(dataServiceMock);
+//    }
     @Test
     public void calculateSumUsingDataService_basic(){
 
