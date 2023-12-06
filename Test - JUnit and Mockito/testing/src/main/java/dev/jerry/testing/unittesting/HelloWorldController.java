a5a31973c451f3f3a16564743631ef11b21e0571
InnovativeCoder <aashitsingh2010@gmail.com>
2023-12-05 19:34:08 -0500

    completed the testing basic in REST MVC

---
+++ Test - JUnit and Mockito/testing/src/main/java/dev/jerry/testing/unittesting/HelloWorldController.java
@@ -0,0 +1,12 @@
+package dev.jerry.testing.unittesting;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/hello-world")
+    public String helloWorld(){
+        return "Hello World from here";
+    }
+}
