77ca6dd4beb7b6aa0529bb3ed784c629783f49f7
InnovativeCoder <aashitsingh2010@gmail.com>
2024-01-01 09:14:15 -0500

    Removed the hard coded users and added jdbc for spring security

--- Spring boot Advance Security /src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ Spring boot Advance Security /src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -3,13 +3,27 @@ package com.luv2code.springboot.cruddemo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
+    //adding support for JDBC to access out MYSQL Database, removing the hardcoded users
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
+    }
 
+    /*
     @Bean
     public InMemoryUserDetailsManager userDetailsManager(){
 
@@ -21,5 +35,23 @@ public class DemoSecurityConfig {
                 .username("susan").password("{noop}test123").roles("EMPLOYEE","MANAGER","ADMIN").build();
 
         return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+*/
+    // Restricting access based on roles
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(configurer-> configurer.requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE"));
+        http.authorizeHttpRequests(configurer-> configurer.requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE"));
+        http.authorizeHttpRequests(configurer-> configurer.requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER"));
+        http.authorizeHttpRequests(configurer-> configurer.requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER"));
+        http.authorizeHttpRequests(configurer-> configurer.requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN"));
+
+        //use HTTP Basic Authentication
+        http.httpBasic((Customizer.withDefaults()));
+
+        //disable CSRF
+        http.csrf(csrf->csrf.disable());
+
+        return http.build();
     }
 }
